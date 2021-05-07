070e4448fda5eb87d7e0800135822028d7360165
Experience360VR <ryan@experience360.tv>
2021-05-06 17:12:10 -0700

    Added classes to walls and power pellets

--- index.js
+++ index.js
@@ -40,6 +40,14 @@ function createBoard() {
         grid.appendChild(square);
         squares.push(square);
 
+
+        if (layout[i] === 0){
+            squares[i].classList.add('pac-dot')
+        } else if (layout[i] === 1){
+            squares[i].classList.add('wall')
+        } else if (layout[i] === 3){
+            squares[i].classList.add('power-pellet')
+        }
     }
 }
 
